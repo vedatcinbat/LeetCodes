7ab53b7464c54de1ed7b37239624f560c276070e
Vedat Cinbat <cinbatvedat@gmail.com>
2025-02-20 07:42:18 +0300

    removeElement Done

--- Java/LeetCode/src/Solution.java
+++ Java/LeetCode/src/Solution.java
@@ -20,4 +20,17 @@ class Solution {
             return list2;
         }
     }
+
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
 }
