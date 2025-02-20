85b1438ad20bdb567c28ad93fff9ccf35a807750
Vedat Cinbat <cinbatvedat@gmail.com>
2025-02-20 07:21:26 +0300

    MergeTwoSortedList Done

--- Java/LeetCode/src/Main.java
+++ Java/LeetCode/src/Main.java
@@ -1,7 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+        Solution solution = new Solution();
 
+        ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
+        ListNode list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
+
+        solution.mergeTwoLists(list1, list2);
     }
 }
