aebeb85260d699b89fdf60889dee6c9313ef62c6
Vedat Cinbat <cinbatvedat@gmail.com>
2025-02-25 13:20:34 +0300

    [2899]LastVisitedIntegers Done

--- Java/LeetCode/src/Main.java
+++ Java/LeetCode/src/Main.java
@@ -1,10 +1,34 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
+//
+//        ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
+//        ListNode list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
+//
+//        solution.mergeTwoLists(list1, list2);
+//
+//        int[] myNumsList = new int[] {3, 2, 3};
+//
+//        int majorityElement = solution.majorityElement(myNumsList);
+//
+//        System.out.println("Majority Element: "+ majorityElement);
 
-        ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
-        ListNode list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
+        // [7,1,5,3,6,4]
+//        int[] profitArr = new int[] {7, 1, 5, 3, 6, 4};
+//
+//        int maxProfit = solution.maxProfit(profitArr);
+//
+//        System.out.println("Max Profit: "+ maxProfit);
+//
 
-        solution.mergeTwoLists(list1, list2);
+        // [1, 2, -1, -1, -1]
+        int[] nums = new int[] {1, -1, 2, -1, -1};
+        List<Integer> result = solution.lastVisitedIntegers(nums);
+
+        System.out.println(result);
+
+
     }
 }
--- Java/LeetCode/src/Solution.java
+++ Java/LeetCode/src/Solution.java
@@ -92,4 +92,76 @@ class Solution {
 
         return maxProfit;
     }
+
+    // Last Visited Integers
+    // Given an integer array "nums" where nums[i] is either a positive integer or -1.
+    // We need to find for each -1 the respective positive integer, which we call the last visited integer
+    // To achieve this goal, lets define two empty arrays: seen and ans
+    // Start iterating from the beginning of the array nums
+
+    // - If a positive integer is encountered, prepend it to the front of seen
+    // - If -1 is encountered, let "k" be the number of consecutive -1s seen so far (including the current -1)
+    // - If k is less than or equal to the length of seen, append the k-th element of seen to ans
+    // - If k is strictly greater than the length of seen, append -1 to ans
+
+    // Return the array ans
+
+    // nums = [1, 2, -1, -1, -1] output: [2, 1, -1]
+    public List<Integer> lastVisitedIntegers(int[] nums) {
+        List<Integer> seen = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        int k = 0;
+
+        for(int num : nums) {
+
+            if(num > 0) {
+                seen.addFirst(num);
+                k = 0;
+            }else if(num == -1){
+                k++;
+                int seenSize = seen.size();
+                if(k <= seenSize) {
+                    ans.add(seen.get(k-1));
+                }else {
+                    ans.add(-1);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
